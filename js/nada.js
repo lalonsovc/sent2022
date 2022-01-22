9bcc8ce5e028fe82001536b946e151cd1bf3e4db
lalonsovc <81634204+lalonsovc@users.noreply.github.com>
2022-01-21 19:55:59 -0500

    V1

---
+++ js/nada.js
@@ -0,0 +1,35 @@
+function colorn1_1(){
+    n.style.background="#cc33b8";
+}
+function colorn1_2(){
+    n.style.background="black";
+}
+n.addEventListener("click",colorn1_1)
+n.addEventListener("mouseout",colorn1_2)
+
+function colora11_1(){
+    a1.style.background="#cc33b8";
+}
+function colora11_2(){
+    a1.style.background="black";
+}
+a1.addEventListener("click",colora11_1)
+a1.addEventListener("mouseout",colora11_2)
+
+function colord1_1(){
+    d.style.background="#cc33b8";
+}
+function colord1_2(){
+    d.style.background="black";
+}
+d.addEventListener("click",colord1_1)
+d.addEventListener("mouseout",colord1_2)
+
+function colora21_1(){
+    a2.style.background="#cc33b8";
+}
+function colora21_2(){
+    a2.style.background="black";
+}
+a2.addEventListener("click",colora21_1)
+a2.addEventListener("mouseout",colora21_2)
